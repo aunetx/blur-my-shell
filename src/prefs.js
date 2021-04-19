231ff4b48982e5b02005426442dc53d3706df314
aunetx <aunetx@yandex.com>
2021-04-19 14:21:26 +0200

    Added preferences support

--- src/prefs.js
+++ src/prefs.js
@@ -9,119 +9,102 @@ let config = new Settings.Prefs();
 const PrefsWidget = GObject.registerClass({
     GTypeName: 'PrefsWidget',
     Template: Extension.dir.get_child('prefs.ui').get_uri(),
+    InternalChildren: [
+        'sigma_scale',
+        'brightness_scale',
+        'blur_dash',
+        'blur_panel',
+        'blur_overview',
+        'blur_lockscreen',
+        'hacks_level0',
+        'hacks_level1',
+        'hacks_level2',
+    ],
 }, class PrefsWidget extends Gtk.Box {
-
     _init(params = {}) {
         super._init(params);
-        let builder = new Gtk.Builder();
 
         // ! sigma
-        let sigma = config.SIGMA;
-        builder.get_object("sigma_scale").set_value(sigma.get());
+        this._sigma_scale.set_value(config.SIGMA.get());
 
         // ! brightness
-        let brightness = config.BRIGHTNESS;
-        builder.get_object("brightness_scale").set_value(brightness.get());
+        this._brightness_scale.set_value(config.BRIGHTNESS.get());
 
         // ! blur dash
-        let blur_dash = config.BLUR_DASH;
-        builder.get_object("blur_dash").set_active(blur_dash.get());
+        this._blur_dash.set_active(config.BLUR_DASH.get());
 
         // ! blur panel
-        let blur_panel = config.BLUR_PANEL;
-        builder.get_object("blur_panel").set_active(blur_panel.get());
+        this._blur_panel.set_active(config.BLUR_PANEL.get());
 
         // ! blur overview
-        let blur_overview = config.BLUR_OVERVIEW;
-        builder.get_object("blur_overview").set_active(blur_overview.get());
+        this._blur_overview.set_active(config.BLUR_OVERVIEW.get());
 
         // ! blur lockscreen
-        let blur_lockscreen = config.BLUR_LOCKSCREEN;
-        builder.get_object("blur_lockscreen").set_active(blur_lockscreen.get());
+        this._blur_lockscreen.set_active(config.BLUR_LOCKSCREEN.get());
 
         // ! dash hacks
-        let hack_level = config.HACKS_LEVEL;
-        let hack_level_0_button = builder.get_object("hacks_level0")
-        let hack_level_1_button = builder.get_object("hacks_level1")
-        let hack_level_2_button = builder.get_object("hacks_level2")
-
-        if (hack_level.get() == 0) {
-            hack_level_0_button.set_active(true);
+        if (config.HACKS_LEVEL.get() == 0) {
+            this._hacks_level0.set_active(true);
         }
-        else if (hack_level.get() == 1) {
-            hack_level_1_button.set_active(true);
+        else if (config.HACKS_LEVEL.get() == 1) {
+            this._hacks_level1.set_active(true);
         }
-        else if (hack_level.get() == 2) {
-            hack_level_2_button.set_active(true);
+        else if (config.HACKS_LEVEL.get() == 2) {
+            this._hacks_level2.set_active(true);
         }
         else {
             this._log(`hack level out-of-bound: ${hack_level}, defaulting to 1.`);
-            hack_level_1_button.set_active(true);
+            this._hacks_level0.set_active(true);
         }
+    }
 
-        // ! animate overview
-        let animate_overview = config.ANIMATE_OVERVIEW;
-        builder.get_object("animate_overview").set_active(animate_overview.get());
+    sigma_changed(w) {
+        let value = w.get_value();
+        config.SIGMA.set(value);
+    }
 
+    brightness_changed(w) {
+        let value = w.get_value();
+        config.BRIGHTNESS.set(value);
+    }
 
-        // ! connect
-        let SignalHandler = {
-            sigma_changed(w) {
-                let value = w.get_value();
-                sigma.set(value);
-            },
+    blur_dash_toggled(w) {
+        let value = w.get_active();
+        config.BLUR_DASH.set(value);
+    }
 
-            brightness_changed(w) {
-                let value = w.get_value();
-                brightness.set(value);
-            },
+    blur_panel_toggled(w) {
+        let value = w.get_active();
+        config.BLUR_PANEL.set(value);
+    }
 
-            blur_dash_toggled(w) {
-                let value = w.get_active();
-                blur_dash.set(value);
-            },
+    blur_overview_toggled(w) {
+        let value = w.get_active();
+        config.BLUR_OVERVIEW.set(value);
+    }
 
-            blur_panel_toggled(w) {
-                let value = w.get_active();
-                blur_panel.set(value);
-            },
+    blur_lockscreen_toggled(w) {
+        let value = w.get_active();
+        config.BLUR_LOCKSCREEN.set(value);
+    }
 
-            blur_overview_toggled(w) {
-                let value = w.get_active();
-                blur_overview.set(value);
-            },
+    hacks_level0_toggled(w) {
+        let is_active = w.get_active();
+        if (is_active) { config.HACKS_LEVEL.set(0) }
+    }
 
-            blur_lockscreen_toggled(w) {
-                let value = w.get_active();
-                blur_lockscreen.set(value);
-            },
+    hacks_level1_toggled(w) {
+        let is_active = w.get_active();
+        if (is_active) { config.HACKS_LEVEL.set(1) }
+    }
 
-            hacks_level0_toggled(w) {
-                let is_active = w.get_active();
-                if (is_active) { hack_level.set(0) }
-            },
+    hacks_level2_toggled(w) {
+        let is_active = w.get_active();
+        if (is_active) { config.HACKS_LEVEL.set(2) }
+    }
 
-            hacks_level1_toggled(w) {
-                let is_active = w.get_active();
-                if (is_active) { hack_level.set(1) }
-            },
-
-            hacks_level2_toggled(w) {
-                let is_active = w.get_active();
-                if (is_active) { hack_level.set(2) }
-            },
-
-            animate_overview_toggled(w) {
-                let value = w.get_active();
-                animate_overview.set(value);
-            },
-        };
-
-        builder.connect_signals_full((builder, object, signal, handler) => {
-            object.connect(signal, SignalHandler[handler].bind(this));
-        });
-
-        this.add(builder.get_object('main_frame'));
+    _log(str) {
+        log(`[Blur my Shell] ${str}`)
     }
 });
 
