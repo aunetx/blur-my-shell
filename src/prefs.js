924652c42f1ecb003c6b01f632ce1d89622116a2
aunetx <aunetx@yandex.com>
2022-03-28 13:39:56 +0200

    First iteration with libadwaita, does not work yet

--- src/prefs.js
+++ src/prefs.js
@@ -1,99 +1,86 @@
 'use strict';
 
-const { GObject, Gtk, Gio } = imports.gi;
-
+const { Adw, Gdk, GLib, Gtk, GObject, Gio } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
-const Extension = ExtensionUtils.getCurrentExtension();
-const Settings = Extension.imports.settings;
+
+const Me = ExtensionUtils.getCurrentExtension();
+const Settings = Me.imports.settings;
 
 
-var PreferencesDialog = class PreferencesDialog {
-    constructor() {
-        // load our resources
+const Preferences = new Prefs(Keys);
 
-        this.resources = Gio.Resource.load(
-            `${Extension.path}/blur-my-shell.gresource`
-        );
-        Gio.resources_register(this.resources);
+const parse_color_from_setting = function (setting, widget) {
+    let color_string = setting.get();
+    let color_parsed = new Gdk.RGBA;
+    let is_parsed = color_parsed.parse(color_string);
 
-        // Load the CSS file for the settings dialog.
-        /*
-        const styleProvider = Gtk.CssProvider.new();
-        styleProvider.load_from_resource('/css/gtk.css');
-        Gtk.StyleContext.add_provider_for_display(
-        Gdk.Display.get_default(), styleProvider,
-        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
-        */
-
-        // Make sure custom icons are found.
-        /*
-        Gtk.IconTheme.get_for_display(Gdk.Display.get_default()).add_resource_path('/img');
-        */
-
-        // Register the template widgets used in the settings dialog.
-        /*
-        this._registerCustomClasses();
-        */
-
-        this.prefs = new Settings.Prefs();
-        this.builder = new Gtk.Builder();
-
-        // Load the general user interface files.
-        this.builder.add_from_resource(`/ui/prefs.ui`);
-
-        // This is our top-level widget which we will return later.
-        this.widget = this.builder.get_object('settings-widget');
-
-        // Some things can only be done once the widget is shown as we do not have access to
-        // the toplevel widget before.
-        this.widget.connect('realize', widget => {
-            const window = widget.get_root();
-
-            // Show the version number in the title bar.
-            window.set_title(`Blur my Shell ${Me.metadata.version}`);
-
-            // Add the main menu to the title bar.
-            {
-                // Add the menu button to the title bar.
-                const menu = this.builder.get_object('menu-button');
-                window.get_titlebar().pack_end(menu);
-
-                // Populate the menu with actions.
-                const group =
-                    Gio.SimpleActionGroup.new();
-                window.insert_action_group('prefs', group);
-
-                const addAction = (name, uri) => {
-                    const action = Gio.SimpleAction.new(name, null);
-                    action.connect('activate', _ => Gtk.show_uri(null, uri, Gdk.CURRENT_TIME));
-                    group.add_action(action);
-                };
-
-                addAction('homepage', 'https://github.com/aunetx/blur-my-shell');
-                addAction('bugs', 'https://github.com/aunetx/blur-my-shell/issues');
-                addAction('donate-kofi', 'https://ko-fi.com/aunetx');
-                addAction('donate-github', 'https://github.com/sponsors/aunetx');
-            }
-        });
-
-        // As we do not have something like a destructor, we just listen for the destroy
-        // signal of our main widget.
-        this.widget.connect('destroy', _ => {
-            // unregister our resources
-            Gio.resources_unregister(this.resources);
-        });
-    }
-
-    _log(str) {
-        if (config.DEBUG.get())
-            log(`[Blur my Shell] ${str}`);
+    if (is_parsed) {
+        widget.set_rgba(color_parsed);
+    } else {
+        // could not parse color, defaulting to black
+        setting.set('#000000ff');
     }
 };
 
+var MainPage = GObject.registerClass({
+    GTypeName: 'MainPage',
+    Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'main_page.ui'])}`,
+    InternalChildren: [
+        'panel_corners',
+        'panel_corner_color',
+        'panel_radius_adjustment',
+        'panel_opacity_adjustment',
+
+        'screen_corners',
+        'screen_corner_color',
+        'screen_radius_adjustment',
+        'screen_opacity_adjustment',
+
+        'force_extension_values',
+        'debug',
+    ],
+}, class MainPage extends Adw.PreferencesPage {
+    constructor(props = {}) {
+        super(props);
+
+        // Panel corners
+        Preferences.settings.bind('panel-corners', this._panel_corners, 'state', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('panel-corner-radius', this._panel_radius_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('panel-corner-opacity', this._panel_opacity_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.PANEL_CORNER_BACKGROUND_COLOR.changed(_ => {
+            parse_color_from_setting(Preferences.PANEL_CORNER_BACKGROUND_COLOR, this._panel_corner_color);
+        });
+        this._panel_corner_color.connect('color-set', _ => {
+            let color = this._panel_corner_color.rgba.to_string();
+            Preferences.PANEL_CORNER_BACKGROUND_COLOR.set(color);
+        });
+        parse_color_from_setting(Preferences.PANEL_CORNER_BACKGROUND_COLOR, this._panel_corner_color);
+
+        // Screen corners
+        Preferences.settings.bind('screen-corners', this._screen_corners, 'state', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('screen-corner-radius', this._screen_radius_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('screen-corner-opacity', this._screen_opacity_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.SCREEN_CORNER_BACKGROUND_COLOR.changed(_ => {
+            parse_color_from_setting(Preferences.SCREEN_CORNER_BACKGROUND_COLOR, this._screen_corner_color);
+        });
+        this._screen_corner_color.connect('color-set', _ => {
+            let color = this._screen_corner_color.rgba.to_string();
+            Preferences.SCREEN_CORNER_BACKGROUND_COLOR.set(color);
+        });
+        parse_color_from_setting(Preferences.SCREEN_CORNER_BACKGROUND_COLOR, this._screen_corner_color);
+
+        // Advanced
+        Preferences.settings.bind('force-extension-values', this._force_extension_values, 'state', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('debug', this._debug, 'state', Gio.SettingsBindFlags.DEFAULT);
+    }
+});
+
+
 function init() { }
 
-function buildPrefsWidget() {
-    let dialog = new PreferencesDialog;
-
-    return dialog.widget;
+function fillPreferencesWindow(window) {
+    let main_page = new MainPage();
+    window.add(main_page);
+    window.search_enabled = true;
+    window.set_default_size(720, 530);
 }
