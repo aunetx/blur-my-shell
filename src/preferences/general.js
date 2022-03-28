0ffea58e60ab32cf6c4905b3a3669c67853c5454
aunetx <aunetx@yandex.com>
2022-03-29 00:24:36 +0200

    Working general preferences

--- src/preferences/general.js
+++ src/preferences/general.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Adw, GLib, GObject } = imports.gi;
+const { Adw, GLib, GObject, Gio, Gtk } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 
 const Me = ExtensionUtils.getCurrentExtension();
@@ -9,20 +9,25 @@ const { Keys } = Me.imports.conveniences.keys;
 
 const Preferences = new Prefs(Keys);
 
-
 var General = GObject.registerClass({
     GTypeName: 'General',
-    // TODO use gresources to load ui files
     Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'general.ui'])}`,
     InternalChildren: [
         'sigma',
         'brightness',
+        'hack-level',
         'debug',
         'sigma_adjustment',
         'brightness_adjustment',
+        'hack_level_model'
     ],
 }, class General extends Adw.PreferencesPage {
     constructor(props = {}) {
         super(props);
+
+        Preferences.settings.bind('sigma', this._sigma_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('brightness', this._brightness_adjustment, 'value', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('hacks-level', this._hack_level, 'selected', Gio.SettingsBindFlags.DEFAULT);
+        Preferences.settings.bind('debug', this._debug, 'state', Gio.SettingsBindFlags.DEFAULT);
     }
 });
