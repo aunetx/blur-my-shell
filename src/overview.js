c369bbd9ed4aacdb4789f5af7f6a139cd6f03cb2
aunetx <aunetx@yandex.com>
2022-03-28 14:30:34 +0200

    Fix #199

--- src/overview.js
+++ src/overview.js
@@ -24,7 +24,7 @@ var OverviewBlur = class OverviewBlur {
         this.connections.connect(Main.layoutManager._backgroundGroup, 'notify', () => {
             this._log("updated background");
             this.update_backgrounds();
-        })
+        });
 
         // connect to monitors change
         this.connections.connect(Main.layoutManager, 'monitors-changed', () => {
@@ -41,7 +41,7 @@ var OverviewBlur = class OverviewBlur {
         // the try/catch behaviour is used to prevent bugs like #136 and #137
         try {
             this.update_backgrounds();
-        } catch (error) { this._log(`could not blur overview: ${error}`) }
+        } catch (error) { this._log(`could not blur overview: ${error}`); }
 
         // store original methods for restoring them on disable()
         this._origPrepareSwitch = WorkspaceAnimationController.prototype._prepareWorkspaceSwitch;
@@ -55,13 +55,13 @@ var OverviewBlur = class OverviewBlur {
             outerThis._origPrepareSwitch.apply(this, params);
 
             Main.layoutManager.monitors.forEach(monitor => {
-              if (!(Meta.prefs_get_workspaces_only_on_primary() && (monitor != Main.layoutManager.primaryMonitor))) {
-                const bg_actor = outerThis.create_background_actor(monitor);
-                Main.uiGroup.insert_child_above(bg_actor, global.window_group);
+                if (!(Meta.prefs_get_workspaces_only_on_primary() && (monitor != Main.layoutManager.primaryMonitor))) {
+                    const bg_actor = outerThis.create_background_actor(monitor);
+                    Main.uiGroup.insert_child_above(bg_actor, global.window_group);
 
-                // store the actors so that we can delete them later
-                outerThis._workspace_switch_bg_actors.push(bg_actor);
-              }
+                    // store the actors so that we can delete them later
+                    outerThis._workspace_switch_bg_actors.push(bg_actor);
+                }
             });
         };
 
@@ -95,9 +95,15 @@ var OverviewBlur = class OverviewBlur {
     }
 
     create_background_actor(monitor) {
-        let bg_actor = new Meta.BackgroundActor
+        let bg_actor = new Meta.BackgroundActor;
 
         let background = Main.layoutManager._backgroundGroup.get_child_at_index(Main.layoutManager.monitors.length - monitor.index - 1);
+
+        if (!background) {
+            this._log("could not get background for overview");
+            return bg_actor;
+        }
+
         bg_actor.set_content(background.get_content());
 
         let effect = new Shell.BlurEffect({
@@ -130,7 +136,7 @@ var OverviewBlur = class OverviewBlur {
         this._log("removing blur from overview");
         Main.layoutManager.overviewGroup.get_children().forEach(actor => {
             if (actor.constructor.name == 'Meta_BackgroundActor') {
-                Main.layoutManager.overviewGroup.remove_child(actor)
+                Main.layoutManager.overviewGroup.remove_child(actor);
             }
         });
         Main.uiGroup.remove_style_class_name("blurred-overview");
@@ -145,6 +151,6 @@ var OverviewBlur = class OverviewBlur {
 
     _log(str) {
         if (this.prefs.DEBUG.get())
-            log(`[Blur my Shell] ${str}`)
+            log(`[Blur my Shell] ${str}`);
     }
-}
+};
