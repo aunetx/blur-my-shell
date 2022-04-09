041441768591961536a6f176cb224e2058fcb7ab
Aurélien Hamy <aunetx@yandex.com>
2022-04-09 11:42:26 +0200

    Revert buggy ease_property change, fixes #213

--- src/conveniences/utilities.js
+++ src/conveniences/utilities.js
@@ -2,9 +2,6 @@
 
 const GLib = imports.gi.GLib;
 
-/// A quick way to track all timeouts created by ease_property.
-const EaseTimeouts = [];
-
 var easeOutQuad = function (t, begin, end, duration) {
     t /= duration;
     return begin + (t * (2 - t) * (end - begin));
@@ -12,7 +9,7 @@ var easeOutQuad = function (t, begin, end, duration) {
 
 var ease_property = function (object, property, begin, end, duration, update) {
     let start = Date.now();
-    id = GLib.timeout_add(GLib.PRIORITY_DEFAULT, update, () => {
+    GLib.timeout_add(GLib.PRIORITY_DEFAULT, update, () => {
         let time = Date.now() - start;
         if (time < duration) {
             object[property] = easeOutQuad(time, begin, end, duration);
@@ -22,13 +19,4 @@ var ease_property = function (object, property, begin, end, duration, update) {
             return GLib.SOURCE_REMOVE;
         }
     });
-    EaseTimeouts.push(id);
-};
-
-/// Removes all timeouts created by ease_property.
-var clear_ease_timeouts = function () {
-    for (timeout in EaseTimeouts) {
-        GLib.Source.remove(timeout);
-    }
-    EaseTimeouts = [];
 };
