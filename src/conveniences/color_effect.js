592cdd533b6df459403cdcf9c6f94a6d4c1ed549
JugadK <curaww@gmail.com>
2022-04-26 11:20:28 -0700

    [add] parameters to color_effect

--- src/conveniences/color_effect.js
+++ src/conveniences/color_effect.js
@@ -9,12 +9,15 @@ const Preferences = new Prefs(Keys);
 
 
 
+// This function will return a new Clutter Shader Effect that simply mixes a color in, 
+// Clutter Shader Source Code https://github.com/GNOME/clutter/blob/master/clutter/clutter-shader-effect.c
+// GJS Doc https://gjs-docs.gnome.org/clutter10~10_api/clutter.shadereffect
 
-function get_new_color_effect(red,green,blue) {
+function get_new_color_effect(red, green, blue) {
 
   const color_effect = new Clutter.ShaderEffect();
 
-  color_effect.set_shader_source( `
+  color_effect.set_shader_source(`
 
   uniform sampler2D tex;
 
@@ -23,11 +26,13 @@ function get_new_color_effect(red,green,blue) {
     vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
     vec3 image = vec3(c.x,c.y,c.z);
 
-    vec3 mixing_color = vec3(`+ red  + "," + green + "," + blue + `);
+    vec3 mixing_color = vec3(`+ red + "," + green + "," + blue + `);
 
     cogl_color_out = vec4(mix(image, mixing_color,0.4),1.0);
   }
 `);
+
+  
 
 
   return color_effect;
@@ -35,10 +40,39 @@ function get_new_color_effect(red,green,blue) {
 }
 
 
+var ColorEffect = new GObject.registerClass({
+  GTypeName: "Color Effect",
+  Properties: {
+    'red' : GObject.ParamSpec.double(
+      `red`,
+      `red`,
+      `Red value in shader`,
+      GObject.ParamFlags.READWRITE,
+      0.0,
+      1.0,
+      0.4,
+    ),
+    'green' : GObject.ParamSpec.double(
+      `green`,
+      `green`,
+      `Green value in shader`,
+      GObject.ParamFlags.READWRITE,
+      0.0,
+      1.0,
+      0.4,
+    ),
+    'blue' : GObject.ParamSpec.double(
+      `blue`,
+      `blue`,
+      `Blue value in shader`,
+      GObject.ParamFlags.READWRITE,
+      0.0,
+      1.0,
+      0.4,
+    ),
+  }
 
-const example = GObject.registerClass({
-  GTypeName: 'ColorEffect',
-}, class TestEffect extends Clutter.ShaderEffect {
+}, class ColorShader extends Clutter.ShaderEffect {
 
   _init(params = {}) {
 
@@ -99,53 +133,48 @@ const example = GObject.registerClass({
   }
 
 
-/*  vfunc_get_static_shader_source() {
+  vfunc_get_static_shader_source() {
 
-    
-    log(this.red + " abcfd")
-    log(this.green + " abcfd")
-    log(this.blue + " abcfd")
-    log("abcdf")
 
     return `
 
-    uniform sampler2D tex;
+  uniform sampler2D tex;
 
 
-    // RGB to HSV functions taken from https://gist.github.com/983/e170a24ae8eba2cd174f
+  // RGB to HSV functions taken from https://gist.github.com/983/e170a24ae8eba2cd174f
 
-    vec3 rgb2hsv(vec3 c) {
-      vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-      vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-      vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+  vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
 
-      float d = q.x - min(q.w, q.y);
-      float e = 1.0e-10;
-      return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-    }
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+  }
 
-    vec3 hsv2rgb(vec3 c)
-    {
-        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-    }
+  vec3 hsv2rgb(vec3 c)
+  {
+      vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+      vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+      return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+  }
 
-    void main() {
-      vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
-      vec3 color = rgb2hsv(vec3(c.x,c.y,c.z));
+  void main() {
+    vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
+    vec3 color = rgb2hsv(vec3(c.x,c.y,c.z));
 
-      // Saturate Image
-      color.y = color.y;
+    // Saturate Image
+    color.y = color.y;
 
-      vec3 red = vec3(`+ this.red  + "," + this.green + "," + this.blue`);
+    vec3 red = vec3(` + this.red + "," + this.green + "," + this.blue + `);
 
-     cogl_color_out = vec4(mix(hsv2rgb(color), red,0.4),1.0);
-    }
-  `;
+   cogl_color_out = vec4(mix(hsv2rgb(color), red,0.4),1.0);
+  }
+`;
 
-  
-  } */
+
+  }
 
   vfunc_paint_target(paint_node = null, paint_context = null) {
     this.set_uniform_value("tex", 0);
