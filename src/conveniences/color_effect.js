0795b1ea9e4a61f3f3afe4465570bfb0ecba6239
JugadK <curaww@gmail.com>
2022-04-25 23:39:29 -0700

    rework color effect into function not GObject class

--- src/conveniences/color_effect.js
+++ src/conveniences/color_effect.js
@@ -10,41 +10,45 @@ const Preferences = new Prefs(Keys);
 
 
 
+function get_new_color_effect(red,green,blue) {
 
+  const color_effect = new Clutter.ShaderEffect();
+
+  color_effect.set_shader_source( `
+
+  uniform sampler2D tex;
+
+
+  void main() {
+    vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
+    vec3 image = vec3(c.x,c.y,c.z);
+
+    vec3 mixing_color = vec3(`+ red  + "," + green + "," + blue + `);
+
+    cogl_color_out = vec4(mix(image, mixing_color,0.4),1.0);
+  }
+`);
+
+
+  return color_effect;
+
+}
 
 
 
 const example = GObject.registerClass({
-  GTypeName: 'based',
-  Properties: {
-    'red': GObject.ParamSpec.double(
-      'red',
-      'red',
-      'red',
-      GObject.ParamFlags.READWRITE,
-      0.0,
-      1.0,
-      0.4,
-    )
-  }
+  GTypeName: 'ColorEffect',
 }, class TestEffect extends Clutter.ShaderEffect {
 
   _init(params = {}) {
 
-    
-
-
-
-    
-
-    
 
     super._init(params);
 
   }
 
   get red() {
-    // Implementing a default value
+
     if (this._red === undefined)
       this._red = null;
 
@@ -52,25 +56,56 @@ const example = GObject.registerClass({
   }
 
   set red(value) {
-    //if (typeof value !== 'double' && value !== null)
-    //  throw ValueError(`Expected "red" got "${typeof value}"`);
 
-    // Checking if the value has actually changed to avoid a signal emission
     if (this._red !== value) {
       this._red = value;
 
-      // GObjects usually emit GObject::notify when properties change,
-      // although there are exceptions.
       this.notify('red');
     }
   }
 
+  get green() {
 
-  vfunc_get_static_shader_source() {
+    if (this._green === undefined)
+      this._green = null;
+
+    return this._green;
+  }
+
+  set green(value) {
+
+    if (this._green !== value) {
+      this._green = value;
+
+      this.notify('green');
+    }
+  }
+
+  get blue() {
+
+    if (this._blue === undefined)
+      this._blue = null;
+
+    return this._blue;
+  }
+
+  set blue(value) {
+
+    if (this._blue !== value) {
+      this._blue = value;
+
+      this.notify('blue');
+    }
+  }
+
+
+/*  vfunc_get_static_shader_source() {
 
     
-
     log(this.red + " abcfd")
+    log(this.green + " abcfd")
+    log(this.blue + " abcfd")
+    log("abcdf")
 
     return `
 
@@ -103,14 +138,14 @@ const example = GObject.registerClass({
       // Saturate Image
       color.y = color.y;
 
-      vec3 red = vec3(`+ this.red  + `,0.0,0.0);
+      vec3 red = vec3(`+ this.red  + "," + this.green + "," + this.blue`);
 
      cogl_color_out = vec4(mix(hsv2rgb(color), red,0.4),1.0);
     }
   `;
 
   
-  }
+  } */
 
   vfunc_paint_target(paint_node = null, paint_context = null) {
     this.set_uniform_value("tex", 0);
@@ -121,190 +156,3 @@ const example = GObject.registerClass({
     else super.vfunc_paint_target();
   }
 })
-
-const ColorEffect = GObject.registerClass(
-  {
-    nick: "cdsasfdf",
-    Name: "ColorEffect",
-    GTypeName: "ColorShader",
-   // Properties: {
-   //   'red': GObject.ParamSpec.double(
-   //     'red',
-   //     'Example Property',
-   //     'A read-write boolean property',
-   //     GObject.ParamFlags.READWRITE,
-   //     0.4
-   //   ),
-   //   'blue': GObject.ParamSpec.double(
-   //     'blue',
-   //     'Example Property',
-   //     'A read-write boolean property',
-   //     GObject.ParamFlags.READWRITE,
-   //     0.4
-   //   ),
-   //   'green': GObject.ParamSpec.double(
-   //     'green',
-   //     'Example Property',
-   //     'A read-write boolean property',
-   //     GObject.ParamFlags.READWRITE,
-   //     0.4
-   //   ),
-   // },
-  },
-  class ColorShader extends Clutter.ShaderEffect {
-
-    _init(constructProperties = { prefs }) {
-      super._init(constructProperties);
-
-      this.red = prefs.RED;
-      this.green = prefs.GREEN;
-      this.blue = prefs.BLUE;
-
-    }
-
-    get red() {
-      return this._red;
-    }
-
-    set red(value) {
-      this._red = value;
-    }
-
-    get green() {
-      return this._green;
-    }
-
-    set green(value) {
-      this._green = value;
-    }
-
-    get blue() {
-      return this._blue;
-    }
-
-    set blue(value) {
-      this._blue = value;
-    }
-
-    /**
-     * I would like to thank the person who made this extension for showing me this is possible https://github.com/vn971/linux-color-inversion
-     */
-
-    vfunc_get_static_shader_source() {
-      return `
-
-            uniform sampler2D tex;
-
-
-            // RGB to HSV functions taken from https://gist.github.com/983/e170a24ae8eba2cd174f
-
-            vec3 rgb2hsv(vec3 c) {
-              vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-              vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-              vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-
-              float d = q.x - min(q.w, q.y);
-              float e = 1.0e-10;
-              return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-            }
-
-            vec3 hsv2rgb(vec3 c)
-            {
-                vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-                vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-                return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-            }
-
-            void main() {
-              vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
-              vec3 color = rgb2hsv(vec3(c.x,c.y,c.z));
-
-              // Saturate Image
-              color.y = color.y;
-
-              vec3 red = vec3(1.0,0.2,0.2);
-
-             cogl_color_out = vec4(mix(hsv2rgb(color), red,0.1),1.0);
-            }
-          `;
-    }
-
-    vfunc_paint_target(paint_node = null, paint_context = null) {
-      this.set_uniform_value("tex", 0);
-
-      if (paint_node && paint_context)
-        super.vfunc_paint_target(paint_node, paint_context);
-      else if (paint_node) super.vfunc_paint_target(paint_node);
-      else super.vfunc_paint_target();
-    }
-  }
-);
-
-
-
-
-
-//let color_effect = new GObject.registerClass(
-//  {},
-//  class ColorEffect extends Clutter.ShaderEffect {
-//    /**
-//     * I would like to thank the person who made this extension for showing me this is possible https://github.com/vn971/linux-color-inversion
-//     */
-//
-//    vfunc_get_static_shader_source() {
-//      return `
-//           
-//            uniform sampler2D tex;
-//    
-//    
-//            // RGB to HSV functions taken from https://gist.github.com/983/e170a24ae8eba2cd174f
-//    
-//            vec3 rgb2hsv(vec3 c) {
-//              vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-//              vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-//              vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-//    
-//              float d = q.x - min(q.w, q.y);
-//              float e = 1.0e-10;
-//              return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-//            }
-//    
-//            vec3 hsv2rgb(vec3 c)
-//            {
-//                vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-//                vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-//                return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-//            }
-//    
-//            
-//            
-//            void main() {
-//              vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
-//              vec3 color = rgb2hsv(vec3(c.x,c.y,c.z));
-//    
-//              // Saturate Image
-//              color.y =  0.03 + color.y;
-//              
-//              vec3 red = vec3(0.6.0,0,0);
-//            
-//             cogl_color_out = vec4(mix(hsv2rgb(color), red,0.1),1.0);
-//            }
-//          `
-//    }
-//
-//    //vec3 red = vec3(${prefs.red} ,${prefs.green},${prefs.blue});
-//    vfunc_paint_target(paint_node = null, paint_context = null) {
-//      this.set_uniform_value("tex", 0)
-//
-//      if (paint_node && paint_context)
-//        super.vfunc_paint_target(paint_node, paint_context)
-//      else if (paint_node) super.vfunc_paint_target(paint_node)
-//      else super.vfunc_paint_target()
-//    }
-//  }
-//)
-//
-//function new_color_effect() {
-//  return new GObject.g_object_new(color_effect);
-//}
-//
