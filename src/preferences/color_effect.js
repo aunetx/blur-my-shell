7f585f7cd8cd2d4c9ec60a9e558335852fd164b2
JugadK <curaww@gmail.com>
2022-04-29 14:09:29 -0700

    [remove] old coloreffect

--- src/preferences/color_effect.js
+++ src/preferences/color_effect.js
@@ -1,36 +1,3 @@
-//'use strict';
-//
-//const { Adw, GLib, GObject, Gio } = imports.gi;
-//const ExtensionUtils = imports.misc.extensionUtils;
-//
-//const Me = ExtensionUtils.getCurrentExtension();
-//const { Prefs } = Me.imports.conveniences.settings;
-//const { Keys } = Me.imports.conveniences.keys;
-//
-//const Preferences = new Prefs(Keys);
-//
-//
-//var ColorBlur = GObject.registerClass({
-//    GTypeName: 'ColorBlur',
-//    Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'colorblur.ui'])}`,
-//    InternalChildren: [
-//        'color-blur-toggle',
-//        'red',
-//        'green',
-//        'blue',
-//    ],
-//}, class ColorBlur extends Adw.PreferencesPage {
-//    constructor(props = {}) {
-//        super(props);
-//
-//        //Preferences.settings.bind('color-blur-toggle', this._color_blur, 'state', Gio.SettingsBindFlags.DEFAULT);
-//
-//        Preferences.settings.bind('red', this.red, 'value', Gio.SettingsBindFlags.DEFAULT);
-//        //Preferences.settings.bind('green', this.green, 'value', Gio.SettingsBindFlags.DEFAULT);
-//        //Preferences.settings.bind('blue', this.blue, 'value', Gio.SettingsBindFlags.DEFAULT);
-//    }
-//});
-
 'use strict';
 
 const { Adw, GLib, GObject, Gio } = imports.gi;
