3f22932e58a0fc812d9830af920e0ad75b9e35fc
JugadK <curaww@gmail.com>
2022-04-28 20:06:42 -0700

    [rename] color_blur file to follow other file conventions

--- src/preferences/color_effect.js
+++ src/preferences/color_effect.js
@@ -43,18 +43,20 @@ const { Keys } = Me.imports.conveniences.keys;
 const Preferences = new Prefs(Keys);
 
 
-var ColorBlur = GObject.registerClass({
-    GTypeName: 'ColorBlur',
-    Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'colorblur.ui'])}`,
+var ColorEffect = GObject.registerClass({
+    GTypeName: 'Color_Effect',
+    Template: `file://${GLib.build_filenamev([Me.path, 'ui', 'coloreffect.ui'])}`,
     InternalChildren: [
+        'color_blur',
         'red',
         'green',
         'blue',
     ],
-}, class ColorBlur extends Adw.PreferencesPage {
+}, class ColorEffect extends Adw.PreferencesPage {
     constructor(props = {}) {
         super(props);
 
+        Preferences.settings.bind('color-blur', this._color_blur, 'state', Gio.SettingsBindFlags.DEFAULT)
         Preferences.settings.bind('red', this._red, 'value', Gio.SettingsBindFlags.DEFAULT);
         Preferences.settings.bind('green', this._green, 'value', Gio.SettingsBindFlags.DEFAULT);
         Preferences.settings.bind('blue', this._blue, 'value', Gio.SettingsBindFlags.DEFAULT);
