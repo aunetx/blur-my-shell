b313d51588b6c2f1ed919e11ad27539a8e3a8c73
Aurélien Hamy <aunetx@yandex.com>
2024-04-07 13:49:20 +0200

    Fix pipeline selector being reinitialized when changing pipelines list

--- src/preferences/pipelines_management/pipeline_choose_row.js
+++ src/preferences/pipelines_management/pipeline_choose_row.js
@@ -17,9 +17,11 @@ export const PipelineChooseRow = GObject.registerClass({
     initialize(preferences, pipelines_manager, pipelines_page) {
         this.preferences = preferences;
         this.pipelines_manager = pipelines_manager;
+        this.pipelines_page = pipelines_page;
 
         this.create_pipelines_list();
 
+        // display the correct pipeline name in the drop-down instead of their ids
         const closure_func = string_object => {
             const pipeline_id = string_object.get_string();
             if (pipeline_id == 'create_new')
@@ -38,38 +40,43 @@ export const PipelineChooseRow = GObject.registerClass({
             )
         );
 
-        this.preferences.PIPELINE_changed(() => {
-            for (let i = 0; i < this._pipeline_model.n_items; i++) {
-                const pipeline_id = this._pipeline_model.get_string(i);
-                if (pipeline_id == this.preferences.PIPELINE)
-                    this._pipeline_choose.set_selected(i);
-            }
-        });
+        this.preferences.PIPELINE_changed(() => this.on_settings_pipeline_changed());
 
-        this.pipelines_manager.connect(
-            'pipeline-list-changed',
-            () => this.create_pipelines_list()
-        );
+        this.pipelines_manager.connect('pipeline-list-changed', () => this.create_pipelines_list());
 
-        this._pipeline_choose.connect('notify::selected', () => {
-            if (!this._pipeline_choose.selected_item)
-                return;
-            const pipeline_id = this._pipeline_choose.selected_item.get_string();
-            if (pipeline_id == 'create_new') {
-                const id = this.pipelines_manager.create_pipeline(_("New pipeline"));
-                this.preferences.PIPELINE = id;
-            }
-            else
-                this.preferences.PIPELINE = pipeline_id;
-        });
+        this._pipeline_choose.connect('notify::selected', () => this.on_selected_pipeline_changed());
 
         this._pipeline_edit.connect(
             'clicked',
-            () => pipelines_page.open_effects_dialog(this.preferences.PIPELINE)
+            () => this.pipelines_page.open_effects_dialog(this.preferences.PIPELINE)
         );
     }
 
+    on_selected_pipeline_changed() {
+        if (!this._pipeline_choose.selected_item || this._is_creating_pipelines_list)
+            return;
+
+        const pipeline_id = this._pipeline_choose.selected_item.get_string();
+        if (pipeline_id == 'create_new') {
+            const id = this.pipelines_manager.create_pipeline(_("New pipeline"));
+            this.preferences.PIPELINE = id;
+        }
+        else
+            this.preferences.PIPELINE = pipeline_id;
+    }
+
+    on_settings_pipeline_changed() {
+        for (let i = 0; i < this._pipeline_model.n_items; i++) {
+            const pipeline_id = this._pipeline_model.get_string(i);
+            if (pipeline_id == this.preferences.PIPELINE)
+                this._pipeline_choose.set_selected(i);
+        }
+    }
+
     create_pipelines_list() {
+        // prevent the pipeline selector from being updated while re-creating the list
+        this._is_creating_pipelines_list = true;
+
         // remove ancient items
         this._pipeline_model.splice(0, this._pipeline_model.n_items, null);
 
@@ -82,5 +89,9 @@ export const PipelineChooseRow = GObject.registerClass({
             i++;
         }
         this._pipeline_model.append('create_new');
+
+        // now update the drop-down selector
+        this._is_creating_pipelines_list = false;
+        this.on_selected_pipeline_changed();
     }
 });
