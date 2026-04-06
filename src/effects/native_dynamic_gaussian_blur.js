f74b76b3e22b1883297dd7b7db2f81e4f27111f2
Aurélien Hamy <aurelien.hamy@student-cs.fr>
2026-04-06 18:16:34 +0200

    Do not crash if gi://Blur does not exist, use AdwSpinRow instead of GtkScale

--- src/effects/native_dynamic_gaussian_blur.js
+++ src/effects/native_dynamic_gaussian_blur.js
@@ -2,7 +2,14 @@ import GObject from 'gi://GObject';
 
 import * as utils from '../conveniences/utils.js';
 const St = await utils.import_in_shell_only('gi://St');
-const Blur = await utils.import_in_shell_only('gi://Blur');
+
+let BlurOrShell = await utils.import_in_shell_only('gi://Blur');
+let IS_BLUR_MODULE = true;
+if (BlurOrShell === null) {
+    BlurOrShell = await utils.import_in_shell_only('gi://Shell');
+    IS_BLUR_MODULE = false;
+}
+
 
 const DEFAULT_PARAMS = {
     unscaled_radius: 30, brightness: 0.6, corner_radius: 14
@@ -13,15 +20,21 @@ export const NativeDynamicBlurEffect = utils.IS_IN_PREFERENCES ?
     { default_params: DEFAULT_PARAMS } :
     new GObject.registerClass({
         GTypeName: "NativeDynamicBlurEffect"
-    }, class NativeDynamicBlurEffect extends Blur.BlurEffect {
+    }, class NativeDynamicBlurEffect extends BlurOrShell.BlurEffect {
         constructor(params) {
             const { unscaled_radius, brightness, corner_radius, ...parent_params } = params;
-            super({ ...parent_params, mode: Blur.BlurMode.BACKGROUND, ...{ 'corner_radius': corner_radius } });
+            if (IS_BLUR_MODULE)
+                super({ ...parent_params, mode: BlurOrShell.BlurMode.BACKGROUND, ...{ 'corner_radius': corner_radius } });
+            else
+                super({ ...parent_params, mode: BlurOrShell.BlurMode.BACKGROUND });
 
             this._theme_context = St.ThemeContext.get_for_stage(global.stage);
             this._theme_context.connectObject(
                 'notify::scale-factor',
-                _ => this.radius = this.unscaled_radius * this._theme_context.scale_factor,
+                _ => {
+                    this.radius = this.unscaled_radius * this._theme_context.scale_factor;
+                    this.corner_radius = this.unscaled_corner_radius * this._theme_context.scale_factor;
+                },
                 this
             );
 
@@ -41,11 +54,12 @@ export const NativeDynamicBlurEffect = utils.IS_IN_PREFERENCES ?
             this.radius = value * this._theme_context.scale_factor;
         }
 
-        get corner_radius() {
-            return this.get_property?.('corner_radius');
+        get unscaled_corner_radius() {
+            return this._unscaled_corner_radius;
         }
 
-        set corner_radius(value) {
-            this.set_property?.('corner_radius', value);
+        set unscaled_corner_radius(value) {
+            this._unscaled_corner_radius = value;
+            this.corner_radius = value * this._theme_context.scale_factor;
         }
     });
